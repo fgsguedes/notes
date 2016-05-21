fdd7539f6f0f7f0bcd775e7a5770106e52a6bab3
Filipe Guedes <fgs.guedes@gmail.com>
2016-05-21 14:17:53 -0700

    [JMIW] Label and Note as Parcelables

--- app/src/main/kotlin/br/com/hardcoded/notes/domain/model/Label.kt
+++ app/src/main/kotlin/br/com/hardcoded/notes/domain/model/Label.kt
@@ -1,7 +1,28 @@
 package br.com.hardcoded.notes.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Label(
     val id: Long,
     val title: String
-)
+) : Parcelable {
+
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.writeLong(id)
+    dest.writeString(title)
+  }
+
+  override fun describeContents() = 0
+
+  companion object {
+    @JvmField val CREATOR = object : Parcelable.Creator<Label> {
+      override fun newArray(size: Int) = emptyArray<Label>()
+
+      override fun createFromParcel(source: Parcel) = Label(
+          source.readLong(), source.readString()
+      )
+    }
+  }
+}
 
--- app/src/main/kotlin/br/com/hardcoded/notes/domain/model/Note.kt
+++ app/src/main/kotlin/br/com/hardcoded/notes/domain/model/Note.kt
@@ -1,5 +1,7 @@
 package br.com.hardcoded.notes.domain.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import java.util.*
 
 data class Note(
@@ -8,4 +10,33 @@ data class Note(
     val content: String? = null,
     val label: Label? = null,
     val remindOn: Date? = null
-)
+) : Parcelable {
+
+  override fun writeToParcel(dest: Parcel, flags: Int) {
+    dest.apply {
+      writeLong(id)
+      writeString(title)
+      writeString(content)
+      writeParcelable(label, 0)
+      writeValue(remindOn?.time)
+    }
+  }
+
+  override fun describeContents() = 0
+
+  companion object {
+    @JvmField val CREATOR = object : Parcelable.Creator<Note> {
+      override fun newArray(size: Int) = emptyArray<Note>()
+
+      override fun createFromParcel(source: Parcel) = Note(
+          source.readLong(),
+          source.readString(),
+          source.readString(),
+          source.readParcelable(Label::class.java.classLoader),
+          source.readValue(Long::class.java.classLoader)?.let {
+            Date(it as Long)
+          }
+      )
+    }
+  }
+}
