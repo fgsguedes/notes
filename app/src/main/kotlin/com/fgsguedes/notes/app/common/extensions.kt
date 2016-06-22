047e14f1a9dd6dbb5fcfb6feddf228e8122ec543
Filipe Guedes <fgs.guedes@gmail.com>
2016-06-22 00:42:28 -0300

    Removed unused and simplified one extension

--- app/src/main/kotlin/com/fgsguedes/notes/app/common/extensions.kt
+++ app/src/main/kotlin/com/fgsguedes/notes/app/common/extensions.kt
@@ -1,27 +1,17 @@
 package com.fgsguedes.notes.app.common
 
 import android.support.v7.app.AppCompatActivity
-import android.widget.EditText
 import com.fgsguedes.notes.app.App
 import com.fgsguedes.notes.app.common.injection.component.ApplicationComponent
 
 val AppCompatActivity.applicationComponent: ApplicationComponent
   get() = (application as App).applicationComponent
 
-
-val EditText.nullableStringContent: String?
-  get() = text.toString().let {
-    when {
-      it.isNotBlank() -> it.trim()
-      else -> null
-    }
-  }
-
 val <T : Any> T.TAG: String
   get() = javaClass.simpleName
 
 // To be used with view-click in conjunction with single-line `when` clauses
-fun AppCompatActivity.returningTrue(f: () -> Unit): Boolean {
+fun returningTrue(f: () -> Unit): Boolean {
   f()
   return true
 }
