baa0c310071a05453801189a48eb6b3fb8e63c00
Filipe Guedes <fgs.guedes@gmail.com>
2020-01-21 00:39:41 +0100

    Updated libraries

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,10 +1,10 @@
 object Version {
     const val kotlin = "1.3.61"
 
-    const val gradle = "6.0.1"
+    const val gradle = "6.1"
 
     const val versionCode = 1
-    const val versionName = "0.0.1"
+    const val versionName = "2.0.0"
 
     const val compileSdk = 29
     const val buildTools = "29.0.2"
@@ -14,13 +14,13 @@ object Version {
 }
 
 object Deps {
-    private const val roomVersion = "2.2.2"
+    private const val roomVersion = "2.2.3"
     private const val coroutinesVersion = "1.3.3"
-    private const val lifecycleVersion = "2.2.0-rc02"
+    private const val lifecycleVersion = "2.2.0-rc03"
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.3"
     const val kotlinGradlePlugin = "gradle-plugin"
-    const val dexcountGradlePlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:1.0.0"
+    const val dexcountGradlePlugin = "com.getkeepsafe.dexcount:dexcount-gradle-plugin:1.0.2"
 
     const val kotlinStdlib = "stdlib-jdk8"
 
@@ -46,7 +46,7 @@ object Deps {
     const val roomKtx = "androidx.room:room-ktx:$roomVersion"
     const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
 
-    const val junit = "org.junit.jupiter:junit-jupiter:5.5.2"
+    const val junit = "org.junit.jupiter:junit-jupiter:5.6.0"
     const val assertJ = "org.assertj:assertj-core:3.14.0"
     const val mockito = "org.mockito:mockito-core:3.2.4"
     const val mockitoInline = "org.mockito:mockito-inline:3.2.4"
