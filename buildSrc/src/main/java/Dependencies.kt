442088fa52747f84727d7e883f0a0f5230c2d665
Filipe Guedes <fgs.guedes@gmail.com>
2020-02-17 20:54:21 +0100

    Updated dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,13 +1,13 @@
 object Version {
     const val kotlin = "1.3.61"
 
-    const val gradle = "6.1"
+    const val gradle = "6.2"
 
-    const val versionCode = 1
-    const val versionName = "2.0.0"
+    const val versionCode = 2
+    const val versionName = "2.0.1"
 
     const val compileSdk = 29
-    const val buildTools = "29.0.2"
+    const val buildTools = "29.0.3"
 
     const val minSdk = 21
     const val targetSdk = 29
@@ -16,7 +16,7 @@ object Version {
 object Deps {
     private const val roomVersion = "2.2.3"
     private const val coroutinesVersion = "1.3.3"
-    private const val lifecycleVersion = "2.2.0-rc03"
+    private const val lifecycleVersion = "2.2.0"
 
     const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.3"
     const val kotlinGradlePlugin = "gradle-plugin"
@@ -26,19 +26,19 @@ object Deps {
 
     const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion"
 
-    const val androidxCore = "androidx.core:core-ktx:1.1.0"
+    const val androidxCore = "androidx.core:core-ktx:1.2.0"
 
     const val appCompat = "androidx.appcompat:appcompat:1.1.0"
-    const val activityKtx = "androidx.activity:activity-ktx:1.0.0"
+    const val activityKtx = "androidx.activity:activity-ktx:1.1.0"
     const val recyclerView = "androidx.recyclerview:recyclerview:1.1.0"
     const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
-    const val coordinatorLayout = "androidx.coordinatorlayout:coordinatorlayout:1.0.0"
+    const val coordinatorLayout = "androidx.coordinatorlayout:coordinatorlayout:1.1.0"
 
     const val lifecycleRuntime = "androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion"
     const val lifecycleLivedata = "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion"
     const val lifecycleViewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion"
 
-    const val googleMaterial = "com.google.android.material:material:1.0.0"
+    const val googleMaterial = "com.google.android.material:material:1.1.0"
 
     const val timber = "com.jakewharton.timber:timber:4.7.1"
 
@@ -47,7 +47,7 @@ object Deps {
     const val roomCompiler = "androidx.room:room-compiler:$roomVersion"
 
     const val junit = "org.junit.jupiter:junit-jupiter:5.6.0"
-    const val assertJ = "org.assertj:assertj-core:3.14.0"
+    const val assertJ = "org.assertj:assertj-core:3.15.0"
     const val mockito = "org.mockito:mockito-core:3.2.4"
     const val mockitoInline = "org.mockito:mockito-inline:3.2.4"
     const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
